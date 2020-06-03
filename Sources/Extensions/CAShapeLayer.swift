99756b6bcbd71ad54520f6a6452e5dbadf184799
irel <ireldeveloper@gmail.com>
2020-06-03 13:48:09 +0500

    renamed & added extensions

---
+++ Sources/Extensions/CAShapeLayer.swift
@@ -0,0 +1,21 @@
+//
+//  CAShapeLayer.swift
+//  TelegramColorPicker
+//
+//  Created by Kirill Pustovalov on 03.06.2020.
+//  Copyright © 2020 Kirill Pustovalov. All rights reserved.
+//
+
+import UIKit
+
+extension CAShapeLayer {
+    func imageFromLayer() -> UIImage? {
+        UIGraphicsBeginImageContextWithOptions(self.path!.boundingBox.size,
+                                               self.isOpaque, UIScreen.main.scale)
+        defer { UIGraphicsEndImageContext() }
+        guard let context = UIGraphicsGetCurrentContext() else { return nil }
+        
+        self.render(in: context)
+        return UIGraphicsGetImageFromCurrentImageContext()
+    }
+}
---
+++ Sources/Extensions/UIColor.swift
@@ -0,0 +1,34 @@
+//
+//  UIColor.swift
+//  TelegramColorPicker
+//
+//  Created by Kirill Pustovalov on 03.06.2020.
+//  Copyright © 2020 Kirill Pustovalov. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    func setBrightness(withBrightness brightness: CGFloat) -> UIColor {
+        var hue: CGFloat = 0, saturation: CGFloat = 0, currentBrightness: CGFloat = 0, alpha: CGFloat = 0
+        
+        if getHue(&hue, saturation: &saturation, brightness: &currentBrightness, alpha: &alpha) {
+            currentBrightness += (brightness - 1.0)
+            currentBrightness = max(min(brightness, 1.0), 0.0)
+            
+            return UIColor(hue: hue, saturation: saturation, brightness: brightness, alpha: alpha)
+        }
+        
+        return self
+    }
+    func toHex() -> String? {
+        var unsafeRed: CGFloat = 0.0, unsafeGreen: CGFloat = 0.0, unsafeBlue: CGFloat = 0.0
+        getRed(&unsafeRed, green: &unsafeGreen, blue: &unsafeBlue, alpha: nil)
+        
+        let red = lroundf(Float(unsafeRed) * 255)
+        let green = lroundf(Float(unsafeGreen) * 255)
+        let blue = lroundf(Float(unsafeBlue) * 255)
+        
+        return "#\(String(format: "%02lX%02lX%02lX", red, green, blue))"
+    }
+}
