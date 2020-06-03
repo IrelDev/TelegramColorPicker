49a64a288adf58148142b9bd90acb4c8662fbafa
irel <ireldeveloper@gmail.com>
2020-06-03 13:37:17 +0500

    Initial Commit

---
+++ TelegramColorPicker/TelegramColorPicker.h
@@ -0,0 +1,19 @@
+//
+//  TelegramColorPicker.h
+//  TelegramColorPicker
+//
+//  Created by Kirill Pustovalov on 03.06.2020.
+//  Copyright © 2020 Kirill Pustovalov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TelegramColorPicker.
+FOUNDATION_EXPORT double TelegramColorPickerVersionNumber;
+
+//! Project version string for TelegramColorPicker.
+FOUNDATION_EXPORT const unsigned char TelegramColorPickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TelegramColorPicker/PublicHeader.h>
+
+
