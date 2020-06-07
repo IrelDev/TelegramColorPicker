53ca010986bda84cb3fb2b6c33498048269f190a
IrelDev <ireldeveloper@gmail.com>
2020-06-07 11:03:29 +0500

    fixed circle movement & changed constant enum to struct

--- Sources/Views/ColorPickerView.swift
+++ Sources/Views/ColorPickerView.swift
@@ -9,10 +9,12 @@
 import UIKit
 
 @IBDesignable public class ColorPickerView: UIView, BrightnessPickerDelegate {
-    enum Constant: CGFloat {
-        case size = 1.0
-        case saturation = 2.0
-        case circleSize = 50
+    struct Constant {
+        static let size: CGFloat = 1.0
+        static let saturation: CGFloat = 2.0
+        static let circleSize: CGFloat = 50.0
+        static let minimumColorValue: CGFloat = 0.0
+        static let maximumColorValue: CGFloat = 1.0
     }
     internal weak var delegate: ColorPickerDelegate?
     
@@ -45,7 +47,7 @@ import UIKit
         addGestureRecognizer(gestureRecognizer)
     }
     private func setupCircleLayer() {
-        let circle = UIBezierPath(ovalIn: CGRect(x: 0, y: 0, width: Constant.circleSize.rawValue, height: Constant.circleSize.rawValue))
+        let circle = UIBezierPath(ovalIn: CGRect(x: 0, y: 0, width: Constant.circleSize, height: Constant.circleSize))
         let path = circle.cgPath
         circleLayer.path = path
         circleLayer.lineWidth = 5
@@ -56,31 +58,31 @@ import UIKit
     func calculateSaturation(withY y: CGFloat) -> CGFloat {
         let saturation = 1 - y / bounds.height
         
-        guard saturation >= 0 else { return 0 }
-        guard saturation <= 1 else { return 1 }
+        guard saturation >= Constant.minimumColorValue else { return Constant.minimumColorValue }
+        guard saturation <= Constant.maximumColorValue else { return Constant.maximumColorValue }
         
         return saturation
     }
     func calculateHue(withX x: CGFloat) -> CGFloat {
         let hue = x / bounds.width
         
-        guard hue >= 0 else { return 0 }
-        guard hue <= 1 else { return 1 }
+        guard hue >= Constant.minimumColorValue else { return Constant.minimumColorValue }
+        guard hue <= Constant.maximumColorValue else { return Constant.maximumColorValue }
         
         return hue
     }
     public override func draw(_ rect: CGRect) {
         let context = UIGraphicsGetCurrentContext()
         
-        for demensionY: CGFloat in stride(from: 0.0 ,to: rect.height, by: Constant.size.rawValue) {
+        for demensionY: CGFloat in stride(from: 0.0 ,to: rect.height, by: Constant.size) {
             let saturation = calculateSaturation(withY: demensionY)
             
-            for demensionX: CGFloat in stride(from: 0.0 ,to: rect.width, by: Constant.size.rawValue) {
+            for demensionX: CGFloat in stride(from: 0.0 ,to: rect.width, by: Constant.size) {
                 let hue = calculateHue(withX: demensionX)
-                let color = UIColor(hue: hue, saturation: saturation, brightness: 1.0, alpha: 1.0)
+                let color = UIColor(hue: hue, saturation: saturation, brightness: Constant.maximumColorValue, alpha: Constant.maximumColorValue)
                 
                 context!.setFillColor(color.cgColor)
-                context!.fill(CGRect(x: demensionX, y: demensionY, width: Constant.size.rawValue, height: Constant.size.rawValue))
+                context!.fill(CGRect(x: demensionX, y: demensionY, width: Constant.size, height: Constant.size))
             }
         }
         moveCircleToInitialPosition()
@@ -91,10 +93,10 @@ import UIKit
         self.color = colorAtInitialCirclePoint
         
         moveCircle(to: initialCirclePoint, with: color.cgColor)
-        circleLocation = circleLayer.position
         delegate?.colorTouched(sender: nil, withColor: color, atPoint: initialCirclePoint)
     }
     private func moveCircle(to position: CGPoint, with color: CGColor) {
+        circleLocation = position
         guard let shapeBounds = circleLayer.path?.boundingBox else { return }
         let offsetX = shapeBounds.width / 2
         let offsetY = shapeBounds.height / 2
@@ -143,14 +145,14 @@ import UIKit
         }
     }
     private func getColorAtPoint(point: CGPoint) -> UIColor {
-        let roundedPoint = CGPoint(x: Constant.size.rawValue * CGFloat(Int(point.x / Constant.size.rawValue)),
-                                   y: Constant.size.rawValue * CGFloat(Int(point.y / Constant.size.rawValue)))
+        let roundedPoint = CGPoint(x: Constant.size * CGFloat(Int(point.x / Constant.size)),
+                                   y: Constant.size * CGFloat(Int(point.y / Constant.size)))
         let y = roundedPoint.y
         
         let saturation = calculateSaturation(withY: y)
         let hue = calculateHue(withX: roundedPoint.x)
 
-        return UIColor(hue: hue, saturation: saturation, brightness: 1.0, alpha: 1.0)
+        return UIColor(hue: hue, saturation: saturation, brightness: Constant.maximumColorValue, alpha: Constant.maximumColorValue)
     }
     func brightnessValueChanged(withValue value: Float) {
         brightness = CGFloat(value)
